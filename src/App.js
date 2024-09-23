145c8f4026823906b1be705bfd3a2d7d3999ce1a
M karthik <95424179+KarthikM03@users.noreply.github.com>
2024-09-23 13:11:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Deemo from './Deemo.js';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   // let words = ['karthik','mental','kavya','mouni','psyco']
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
     <Deemo />
+    <SpeedInsights />
     </div>
   );
 }
